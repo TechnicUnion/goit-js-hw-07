40ca32c3eabeca06b015a15318996b06efd6d97d
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-09-18 21:02:30 +0300

    adds gallery task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryItemsList = createGalleryList(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryItemsList);
+
+gallery.addEventListener("click", onImgClik);
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+    .map(({ preview }) => {
+      return `<div class="gallery__item">
+     <a class="gallery__link" href="${preview}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${preview}"
+         alt="Image description"
+       />
+     </a>
+   </div>`;
+    })
+    .join("");
+}
+
+function onImgClik() {}
