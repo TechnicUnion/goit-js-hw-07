922fb963bee8413f7935804d8f2cfbceffcce03d
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-09-20 22:55:20 +0300

    adds gallery tak 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryItemsList = createGalleryList(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryItemsList);
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="Image description" />
+</a>`;
+    })
+    .join("");
+}
